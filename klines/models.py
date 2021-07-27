3b2ce0b39d99c4834c55a582601512b50856c631
Bastien Poidevain <bastien@fizzer.com>
2021-07-27 23:11:51 +0200

    Add Pair, Interval and Pair to database

--- klines/models.py
+++ klines/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Pair(models.Model):
+    symbol = models.CharField(max_length=40, unique=True)
+    base_asset = models.CharField(max_length=20)
+    quote_asset = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.symbol
+
+class Interval(models.Model):
+    symbol = models.CharField(max_length=3, unique=True)
+
+    def __str__(self):
+        return self.symbol
+
+class Kline(models.Model):
+    pair = models.ForeignKey(Pair, on_delete=models.CASCADE, null=True)
+    interval = models.ForeignKey(Interval, on_delete=models.CASCADE, null=True)
+    opened_at = models.DateTimeField()
+    closed_at = models.DateTimeField()
+    open_price = models.DecimalField(max_digits=19, decimal_places=8)
+    high_price = models.DecimalField(max_digits=19, decimal_places=8)
+    low_price = models.DecimalField(max_digits=19, decimal_places=8)
+    close_price = models.DecimalField(max_digits=19, decimal_places=8)
+    volume = models.DecimalField(max_digits=19, decimal_places=8)
+    quote_volume = models.DecimalField(max_digits=19, decimal_places=8)
+    taker_buy_base_volume = models.DecimalField(max_digits=19, decimal_places=8)
+    taker_buy_quote_volume = models.DecimalField(max_digits=19, decimal_places=8)
